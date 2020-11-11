4c5d3722b923c72686ed786093c8bd3a6b987912
right42 <chjm452@naver.com>
2020-11-11 23:41:33 +0900

    properties validated

--- src/main/java/me/right/bootstudy/properties/PersonProperties.java
+++ src/main/java/me/right/bootstudy/properties/PersonProperties.java
@@ -4,12 +4,17 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotEmpty;
 
 @Getter @Setter
 @Component
 @ConfigurationProperties("person")
+@Validated
 public class PersonProperties {
 
+    @NotEmpty
     private String name;
 
     private int age;
