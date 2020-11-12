fd513496f75b486cea884868b3f22be5c347a0ad
right42 <chjm452@naver.com>
2020-11-12 23:02:47 +0900

    resource loader & converter

--- src/main/java/me/right/bootstudy/BootStudyApplication.java
+++ src/main/java/me/right/bootstudy/BootStudyApplication.java
@@ -1,14 +1,22 @@
 package me.right.bootstudy;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import me.right.bootstudy.properties.PersonProperties;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.Validator;
+
+import java.nio.file.Files;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 @EnableAsync
@@ -22,15 +30,24 @@ public class BootStudyApplication {
 
     @Component
     @RequiredArgsConstructor
+    @Slf4j
     public static class AppRunner implements ApplicationRunner {
 
         private final PersonProperties personProperties;
 
+        private final ResourceLoader resourceLoader;
+
+        private final ConversionService conversionService;
+
         @Override
         public void run(ApplicationArguments args) throws Exception {
-            System.out.println("============================");
-            System.out.println(personProperties.getName());
-            System.out.println("============================");
+            Resource bookYmlResource = resourceLoader.getResource("classpath:book.yml");
+
+            try (Stream<String> lines = Files.lines(bookYmlResource.getFile().toPath())) {
+                lines.forEach(log::info);
+            }
+
+
         }
     }
 
