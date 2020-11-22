43f67ccbc98df08ef5224cc7eed6fe674e9a3a85
right42 <chjm452@naver.com>
2020-11-22 22:24:49 +0900

    test code modify

--- src/test/java/me/right/bootstudy/async/AsyncTest.java
+++ src/test/java/me/right/bootstudy/async/AsyncTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.concurrent.CompletableFuture;
@@ -11,7 +12,6 @@ import java.util.concurrent.ExecutionException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class AsyncTest {
 
